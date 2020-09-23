d907c5c6953c503b211248a28d9ed15de8c2e5d6
Belzebub <neitteim@gmail.com>
2020-09-23 21:00:28 +0400

    Added bound service

--- app/src/main/java/com/dm/servicedemo/MainActivity.kt
+++ app/src/main/java/com/dm/servicedemo/MainActivity.kt
@@ -2,27 +2,85 @@ package com.dm.servicedemo
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.media.MediaPlayer
+import android.content.ServiceConnection
+import android.os.Binder
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
+import android.os.IBinder
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 const val CHANNEL_DEFAULT_IMPORTANCE = "service channel"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binder: MusicService.MusicBinder
+    private var bound: Boolean = false
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            bound = false
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            if (service != null) {
+                binder = service as MusicService.MusicBinder
+                bound = true
+                updatePlayer()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         createNotificationChannel()
 
         playBtn.setOnClickListener {
-            val intent = Intent(this, MusicService::class.java)
-            startService(intent)
+            if (bound) {
+                binder.play()
+                updatePlayer()
+            } else {
+                val intent = Intent(this, MusicService::class.java)
+                startService(intent)
+                bindService(intent, connection, Context.BIND_AUTO_CREATE)
+            }
         }
+
+        pauseBtn.setOnClickListener {
+            if (bound) {
+                binder.pause()
+                updatePlayer()
+            }
+        }
+    }
+
+    private fun updatePlayer() {
+        if (binder.isPlaying) {
+            playBtn.isEnabled = false
+            pauseBtn.isEnabled = true
+        } else {
+            playBtn.isEnabled = true
+            pauseBtn.isEnabled = false
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Bind to LocalService
+        Intent(this, MusicService::class.java).also { intent ->
+            bindService(intent, connection, 0)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
+        bound = false
     }
 
     private fun createNotificationChannel() {
--- app/src/main/java/com/dm/servicedemo/MusicService.kt
+++ app/src/main/java/com/dm/servicedemo/MusicService.kt
@@ -4,6 +4,7 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
 import android.provider.Settings
 import androidx.core.app.NotificationCompat
@@ -11,6 +12,29 @@ import androidx.core.app.NotificationCompat
 class MusicService : Service() {
 
     private lateinit var player: MediaPlayer
+
+    // Binder given to clients
+    private val binder = MusicBinder()
+
+    inner class MusicBinder : Binder() {
+        fun play() {
+            player.start()
+        }
+
+        fun pause() {
+            player.pause()
+        }
+
+        val position
+        get() = player.currentPosition
+
+        val isPlaying
+        get() = player.isPlaying
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         if (intent?.action == "stop") {
@@ -28,20 +52,20 @@ class MusicService : Service() {
             val pendingIntent =
                 PendingIntent.getService(this, 0, stopIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
+            val activityIntent = Intent(this, MainActivity::class.java)
+            val pendingActionIntent = PendingIntent.getActivity(this, 0, activityIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
             val notification = NotificationCompat.Builder(this, CHANNEL_DEFAULT_IMPORTANCE)
                 .setContentTitle("Playing alarm")
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .addAction(0, "stop", pendingIntent)
+                .setContentIntent(pendingActionIntent)
                 .build()
 
             startForeground(123, notification)
         }
         return START_STICKY
-    }
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
-
     }
 
     override fun onDestroy() {
